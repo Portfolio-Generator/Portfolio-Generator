41f033b3e69315053fd9838deb9f7235b48a081b
Vicky Loebel <vickyloebel@gmail.com>
2021-05-11 14:52:39 -0700

    untested client mutations login, create user, update user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -6,77 +6,43 @@ export const LOGIN_USER = gql`
       token
       user {
         _id
-        username
+        firstname
+        lastname
       }
     }
   }
 `;
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
+export const CREATE_USER = gql`
+  mutation createUser($email: String!, $password: String!, $firstname: String!, $lastname: String!,
+    $phone: String, headshot:String, aboutMe: String) {
+      createUser(email: $email, password: $password, firstname: $firstname, lastname: $lastname,
+        phone:$phone, headshot: $headshot, aboutMe: $aboutMe ) {
+          token
+          user {
+            _id
+            firstname
+            lastname
+            }
       }
-    }
   }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addProject($ProjectTitle: String!) {
-    addProject(ProjectTitle: $ProjectTitle) {
-      _id
-      ProjectTitle
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-      }
+export const UPDATE_USER = gql`
+mutation updateUser($email: String!, $password: String!, $firstname: String!, $lastname: String!,
+  $phone: String, headshot:String, aboutMe: String) {
+    createUser(email: $email, password: $password, firstname: $firstname, lastname: $lastname,
+      phone:$phone, headshot: $headshot, aboutMe: $aboutMe ) {
+        token
+        user {
+          _id
+          firstname
+          lastname
+        }
     }
-  }
+}
 `;
 
-export const ADD_REACTION = gql`
-  mutation addReaction($projectId: ID!, $reactionBody: String!) {
-    addReaction(projectId: $projectId, reactionBody: $reactionBody) {
-      _id
-      reactionCount
-      reactions {
-        _id
-        reactionBody
-        createdAt
-        username
-      }
-    }
-  }
-`;
 
-export const ADD_FRIEND = gql`
-  mutation addFriend($id: ID!) {
-    addFriend(friendId: $id) {
-      _id
-      username
-      friendCount
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
 
-export const REMOVE_FRIEND = gql`
-  mutation removeFriend($id: ID!) {
-    removeFriend(id: $id) {
-      _id
-      username
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
+
