ae49f706727076bd99ff7b8353573da1f130578b
Vicky Loebel <vickyloebel@gmail.com>
2021-05-12 12:32:29 -0700

    mockclient merged with queriesandmutations

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="flex-column justify-flex-start min-100-vh">
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
           <div className="container flex-row justify-space-between-lg justify-center align-center">
-            <h1>Dev Test - this h1 is in app.js</h1>
+            <h1>Dev Test 2 - this h1 is in app.js</h1>
           </div>
         </header>
         <div className="container">
