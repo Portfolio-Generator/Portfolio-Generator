857cca8f50cf08b032596a3d4c2d171562c04c8a
Vicky Loebel <vickyloebel@gmail.com>
2021-05-13 16:11:51 -0700

    signup and login form workking - token error on server

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,45 @@
-import React, { useState } from 'react';
-// import Auth from './utils/auth';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import NavTabs from './components/Header/NavTabs';
+
+import Navbar from './components/Header/Navbar';
 import Footer from './components/Footer/Footer';
 import About from './pages/About';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 const client = new ApolloClient({
-  uri: '/graphql'
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: '/graphql',
 });
 
-function App() {
-
-  // Using useState, set the default value for currentPage to 'Home'
-  const [currentPage, handlePageChange] = useState('Home');
-
-  // The renderPage method uses a switch statement to render the appropriate current page
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'About':
-        return <About />;
-      case 'Home':
-        return <Home />;
-      case 'Login':
-        return <Login />;
-        case 'Logout':
-          return <Logout />;
-      default:
-        return <Home />;
-    }
-  };
-
-  return (
-    <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <div className="bg-secondary mb-4 py-2 flex-row align-center">
-          <div>
-            {/* Pass the state value and the setter as props to NavTabs */}
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-            {/* Call the renderPage function passing in the currentPage */}
-            <hr></hr>
-            <div className="page-container">{renderPage(currentPage)}</div>
-            <div>
-              <Footer />
-            </div>
-          </div>
-        </div>
-        </div>
-    </ApolloProvider>
-  );
-}
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/login' component={Login} />
+              <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+            </Switch>
+            <Footer />
+          </>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+  
 
 export default App;
