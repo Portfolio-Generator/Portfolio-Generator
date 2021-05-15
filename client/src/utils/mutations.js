80d33c4140787a9cb6ad45afd275769a0a81eb49
Stephanie Giel <stephaniegiel@gmail.com>
2021-05-14 18:14:46 -0700

    tried to update mutations.js file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,30 +21,51 @@ export const CREATE_USER = gql`
             _id
             firstname
             lastname
-            }
+          }
       }
   }
 `;
 
 export const UPDATE_USER = gql`
-mutation updateUser($email: String!, $password: String!, $firstname: String!, $lastname: String!,
-  $phone: String, $headshot:String, $aboutMe: String) {
+mutation updateUser($email: String!, $firstname: String, $lastname: String,
+  $phone: String, $headshot:String, $aboutMe: String, $devSkills: [String], $colorPref: Int, 
+  $fontPref: Int) {
     updateUser(email: $email, password: $password, firstname: $firstname, lastname: $lastname,
-      phone:$phone, headshot: $headshot, aboutMe: $aboutMe ) {
-        token
-        user {
+      phone:$phone, headshot: $headshot, aboutMe: $aboutMe devSkills: $devSkills, colorPref: $colorPref,
+      fontPref: $fontPref) {
           _id
           firstname
           lastname
-        }
-    }
+          phone
+          headshot
+          aboutMe
+          devSkills
+          colorPref
+          fontPref
+          projects {
+            title
+            thumbnail
+            repoLink
+            deployedLink
+            videoLink
+            organization
+            blurb
+            projectSkills
+          }
+          socialMedia {
+            platform
+            icon
+            accountLink
+          }
+      }
+    
 }
 `;
 
 export const ADD_PROJECT = gql`
-mutation addProject($title: String!, $thumbnail: String, $repoLink: String, $deployedLink: String,
+mutation addProject($_id: ID!, $title: String, $thumbnail: String, $repoLink: String, $deployedLink: String,
   $videoLink: String, $organization: String, $blurb: String, $projectSkills: [String]) {
-    addProject(title: $title, thumbnail: $thumbnail, repoLink: $repolink, deployedLink: $deployedLink,
+    addProject(_id: $_id, title: $title, thumbnail: $thumbnail, repoLink: $repolink, deployedLink: $deployedLink,
       videoLink: $videoLink, organization: $organization, blurb: $blurb, projectSkills: $projectSkills ) {
       _id
       firstname
@@ -55,8 +76,13 @@ mutation addProject($title: String!, $thumbnail: String, $repoLink: String, $dep
         thumbnail
         repoLink
         deployedLink
+        videoLink
+        organization
+        blurb
+        projectSkills
       }
     }
+  
 }
 `;
 
@@ -76,38 +102,29 @@ mutation addSocialMedia($platform: String!, $icon: String, $accountLink: String!
 }
 `;
 
-export const EDIT_PROJECT = gql`
-mutation updateProject($_id: ID!, $title: String!, $thumbnail: String, $repoLink: String, $deployedLink: String,
+export const UPDATE_PROJECT = gql`
+mutation updateProject($_id: ID!, $title: String, $thumbnail: String, $repoLink: String, $deployedLink: String,
   $videoLink: String, $organization: String, $blurb: String, $projectSkills: [String]) {
     updateProject(_id: $_id, title: $title, thumbnail: $thumbnail, repoLink: $repolink, deployedLink: $deployedLink,
       videoLink: $videoLink, organization: $organization, blurb: $blurb, projectSkills: $projectSkills ) {
-      _id
-      firstname
-      lastname
-      projects{
         _id
         title
         thumbnail
         repoLink
         deployedLink
-      }
     }
 }
 `;
 
-export const EDIT_SOCIAL_MEDIA = gql`
-mutation updateSocialMedia($_id: ID!, $platform: String!, $icon: String, $accountLink: String!) {
-    updateSocialMedia(_id: $_id, platform: $platform, icon: $icon, accountLink: $accountLink) {
-      _id
-      firstname
-      lastname
-      socialMedia {
-        _id
-        platform
-        icon
-        accountLink
-      }
-    }
+export const UPDATE_SOCIAL_MEDIA = gql`
+mutation updateSocialMedia($platform: String!, $icon: String, $accountLink: String!) {
+  updateSocialMedia(platform: $platform, icon: $icon, accountLink: $accountLink) {
+    _id
+    platform
+    icon
+    accountLink
+    
+  }
 }
 `;
 
@@ -120,6 +137,13 @@ mutation removeProject($_id: ID!) {
       projects {
         _id
         title
+        thumbnail
+        repoLink
+        deployedLink
+        videoLink
+        organization
+        blurb
+        projectSkills
       }
     }
 }
