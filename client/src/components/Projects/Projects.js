21ed09a9a72a2faa0476dbaedd076837531eafa8
Vicky Loebel <vickyloebel@gmail.com>
2021-05-19 19:49:16 -0700

    project list working - shows only fields that have data - DELETE project working

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -10,52 +10,39 @@ import { REMOVE_PROJECT } from '../../utils/mutations';
 
 const Projects = ({ userState, setuserState }) => {
   const [NewProjectRequest, setNewProjectRequest] = useState(false);
-  const [deleteId, setDeleteId]=useState('')
   const [removeProject] = useMutation(REMOVE_PROJECT);
 
-  async function handleDeleteProject(event) {
-    event.preventDefault();
-    console.log('Delete button clicked with Id:', deleteId);
-    //  todo *************************************************************
+  async function handleDeleteProject(deleteId) {
     try { 
-      const data = await removeProject({_id: deleteId })
+      const data = await removeProject({variables: { _id: deleteId }})
       // const data = await removeProject({_id:pId})
-
-    //  todo *************************************************************
-  } catch (err) {
+    } catch (err) {
       console.error(err);
     }
   }
 
-  console.log("VICKY userState", userState)
-
- 
   return (
     <div className="flex-row justify-space-between">
       <div className="container" >
         <div>
-          <Button onClick={() => setNewProjectRequest(true)} style={{ backgroundColor: 'green' }}>Add New Project</Button>
+          <Button onClick={() => setNewProjectRequest(true)} >Add New Project</Button>
           {NewProjectRequest ? (
             <div>
               <NewProject />
             </div>
           ) : null}
           {userState.projects.map((proj) => 
-          <ul key={proj._id} style={{ listStyle: 'none' }}>
-              {/* {setDeleteId(proj._id)} */}
-              <li><Button onClick={handleDeleteProject}>Delete Project</Button></li>
-                {/* <li><Button onClick={()=>handleDeleteProject}>Delete Project</Button></li> */}
+            <ul key={proj._id} style={{ listStyle: 'none' }}>
+              {console.log(proj)}
+              <li><Button onClick={() => handleDeleteProject(proj._id)} >Delete Project</Button></li>
                 <li style={{ fontWeight: 'bold' }}>{proj.title}</li>
-                <li>_id: {proj._id}</li>
-                <li>thumbnail: {proj.thumbnail}</li>
-                <li>repoLink: {proj.repoLink}</li>
-                <li>deployedLink: {proj.deployedLink}</li>
-                <li>videoLink: {proj.videoLink}</li>
-                <li>organization: {proj.organization}</li>
-                <li>description: {proj.blurb}</li>
-                <li>project skills: {proj.projectSkills}</li>
+              {proj.thumbnail ? (<li>thumbnail image: {proj.thumbnail}</li>) : null}
+              {proj.repoLink ? (<li>github repository: {proj.repoLink}</li>) : null}
+              {proj.videoLink ? (<li>video link: {proj.videoLink}</li>) : null}
+              {proj.organization ? (<li>organization: {proj.organization}</li>) : null}
+              {proj.blurb ? (<li>description: {proj.blurb}</li>) : null}
+              {proj.projectSkills ? (<li>skills: {proj.projectSkills}</li>) : null}            
               </ul>
-            
           )
           }
         </div>
