78cf609cfdde121179f0d065fc0080fa42a68867
Kash <kashay@agyeman.me>
2021-05-21 14:07:45 -0700

    user portfolio updated

--- client/src/components/UserPortfolioHtml/UserPortfolioHtml.js
+++ client/src/components/UserPortfolioHtml/UserPortfolioHtml.js
@@ -2,17 +2,17 @@
 const UserPortfolioHtml = ({ userState }) => {
 
   // const socialMediaOptions = [
-  //   { name: 'Discord', icon: 'fab fa-discord' },
-  //   { name: 'GitHub', icon: 'fab fa-github' },
-  //   { name: 'Facebook', icon: 'fab fa-facebook' },
-  //   { name: 'Instagram', icon: 'fab fa-instagram' },
-  //   { name: 'LinkedIn', icon: 'fab fa-linkedin' },
-  //   { name: 'Reddit', icon: 'fab fa-reddit' },
-  //   { name: 'Spotify', icon: 'fab fa-spotify' },
-  //   { name: 'StackOverflow', icon: 'fab fa-stackoverflow' },
-  //   { name: 'Twitch', icon: 'fab fa-twitch' },
-  //   { name: 'Twitter', icon: 'fab fa-twitter' },
-  //   { name: 'YouTube', icon: 'fab fa-youtube' }
+  //   { name: 'Discord', url: 'https://portfolio-generator.github.io/icon-page/discord.svg' },
+  //   { name: 'GitHub', url: 'https://portfolio-generator.github.io/icon-page/github.svg },
+  //   { name: 'Facebook', url: 'https://portfolio-generator.github.io/icon-page/facebook.svg' },
+  //   { name: 'Instagram', url: 'https://portfolio-generator.github.io/icon-page/instagram.svg },
+  //   { name: 'LinkedIn', url: 'https://portfolio-generator.github.io/icon-page/linkedin.svg' },
+  //   { name: 'Reddit', url: 'https://portfolio-generator.github.io/icon-page/reddit.svg' },
+  //   { name: 'Spotify', url: 'https://portfolio-generator.github.io/icon-page/spotify.svg' },
+  //   { name: 'StackOverflow', url: 'https://portfolio-generator.github.io/icon-page/stack-overflow.svg' },
+  //   { name: 'Twitch', url: 'https://portfolio-generator.github.io/icon-page/twitch.svg },
+  //   { name: 'Twitter', url: 'fhttps://portfolio-generator.github.io/icon-page/twitter.svg' },
+  //   { name: 'YouTube', url: 'https://portfolio-generator.github.io/icon-page/youtube.svg' }
   // ];
 
 
@@ -36,7 +36,7 @@ const UserPortfolioHtml = ({ userState }) => {
     { name: 'GitHub Pages', url: 'https://portfolio-generator.github.io/icon-page/github-icon.svg' },
     { name: 'Handlebars', url: 'https://portfolio-generator.github.io/icon-page/handlebars.svg' },
     { name: 'Heroku', url: 'https://portfolio-generator.github.io/icon-page/heroku.svg' },
-    { name: 'HTML5', url: 'https://portfolio-generator.github.io/icon-page/html55.svg' },
+    { name: 'HTML5', url: 'https://portfolio-generator.github.io/icon-page/html-5.svg' },
     { name: 'Java', url: 'https://portfolio-generator.github.io/icon-page/java.svg' },
     { name: 'JavaScript', url: 'https://portfolio-generator.github.io/icon-page/javascript.svg' },
     { name: 'jQuery', url: 'https://portfolio-generator.github.io/icon-page/jquery.svg' },
@@ -115,6 +115,7 @@ const UserPortfolioHtml = ({ userState }) => {
   // START OF LOGIC TO CREATE TEMPLATE LITERAL FOR DEVSKILLS
   let devSkillsString = ``;
   if (userState.devSkills?.length) {
+    console.log("*****", userState.devSkills)
     userState.devSkills.forEach((skill, i) => {
       // includes starting html of devSkills container
       let devSkillsUrl = devSkillsOptions.filter(skillOption => skillOption.name == skill).map(skillOptionUrl => skillOptionUrl.url)[0]
