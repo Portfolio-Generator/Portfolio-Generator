245db3b21a3ade37c5239393135c17cc17f610d5
Dan Arbelo <govepitr@gmail.com>
2021-05-24 10:27:47 -0700

    Updated more text styling and fixed title for user

--- client/src/components/UserPortfolioHtml/UserPortfolioHtml.js
+++ client/src/components/UserPortfolioHtml/UserPortfolioHtml.js
@@ -275,7 +275,7 @@ const UserPortfolioHtml = ({ userState }) => {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>${userState.firstname} ${userState.lastname} Portfolio</title>
+    <title>${userState.firstname} ${userState.lastname}</title>
   </head>
   
   <body>
