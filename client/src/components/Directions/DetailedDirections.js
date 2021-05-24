c364b26adeea203ec1742df6b57271ff9ed7e6eb
Dan Arbelo <govepitr@gmail.com>
2021-05-24 09:54:22 -0700

    updated font positioning w/slight style updates

--- client/src/components/Directions/DetailedDirections.js
+++ client/src/components/Directions/DetailedDirections.js
@@ -17,37 +17,37 @@ const DetailedDirections = () => {
             <h4>Developer Information:</h4>
             <ul className="A">
               <li> First Name, Last Name, and Profile Image are required</li>
-              <li> Enter the Profile Image Filename without a directory path, as in "myHeadShot.jpg"</li>
+              <li> Enter the Profile Image Filename without a directory path, as in <b><i>myHeadShot.jpg</i></b></li>
               <li>Select any developer skills that you want list in your portfolio</li>
-              <li>Click "Save"</li>
+              <li>Click <b><i>Save</i></b></li>
             </ul>
             <h4>Social Media:</h4>
             <ul className="A">
               <li> Social Media links are optional</li>
-              <li> Enter the full url to any social media account you want to include, as in: "https://twitter.com/myAccount"</li>
-              <li>Click "Save".</li>
+              <li> Enter the full url to any social media account you want to include, as in: <b><i>https://twitter.com/myAccount</i></b></li>
+              <li>Click <b><i>Save</i></b>.</li>
             </ul>
             <h4>Projects:</h4>
             <ul className="A">
               <li> Projects are the centerpiece of your portfolio. You will need a preview image file for each included project.</li>
-              <li>Click "Add New Projects"</li>
+              <li>Click <b><i>Add New Projects</i></b></li>
               <li>Enter the Title </li>
-              <li>Provide the full url to any links you choose to include, as in: "https://github.com/myAccount"</li>
-              <li>Enter the required Thumbnail Image Filename without a directory path, as in "myThumbnail1.jpg"</li>
-              <li>Enter a short (required) description of the project</li>
-              <li>Click "Save"</li>
+              <li>Provide the full url to any links you choose to include, as in: <b><i>https://github.com/myAccount</i></b></li>
+              <li>Enter the required Thumbnail Image Filename without a directory path, as in <b><i>myThumbnail1.jpg</i></b></li>
+              <li>Enter a short (<b><i>required</i></b>) description of the project</li>
+              <li>Click <b><i>Save</i></b></li>
             </ul>
             <h4>Downloading Portfolio Zip Files:</h4>
             <ul className="A">
               <li>You can save  portfolio information and come back to it at any time</li>
-              <li>When everthing is ready, click "Create Portfolio Zip File" to download zip files containing html and css</li>
-              <li>CREATE a folder for your portfolio and UNZIP "index.html" file into that folder</li>
-              <li>In the Portfolio folder, create a subfolder named "assets"</li>
-              <li>In the assets folder, create TWO subfolders named "css" and  "images"</li>
-              <li>UNZIP the "styles.css" file into the "css" folder</li>
-              <li>MOVE all the images you are using in your portfolio into the "images" folder</li>
+              <li>When everthing is ready, click <b><i>Create Portfolio Zip File</i></b>. to download zip files containing HTML and CSS</li>
+              <li><b>Create</b> a folder for your portfolio and <b>UNZIP</b> <b><i>index.html</i></b> file into that folder</li>
+              <li>In the Portfolio folder, create a subfolder named <b><i>assets</i></b></li>
+              <li>In the assets folder, create TWO subfolders named <b><i>css</i></b> and  <b><i>images</i></b></li>
+              <li><b>UNZIP</b> the <b><i>styles.css</i></b> file into the <b><i>css</i></b> folder</li>
+              <li><b>Move</b> all the images you are using in your portfolio into the <b><i>images</i></b> folder</li>
 
-              <li>IMPORTANT: Your portfolio assumes images are located in an "assets/images" subfolder and that the css is in an "assets/css" subfolder. You must create this path structure to publish your website </li>
+              <li><strong><u>IMPORTANT</u></strong>: Your portfolio assumes images are located in an <b><i>assets/images</i></b> subfolder and that the css is in an <b><i>assets/css</i></b> subfolder. You must create this path structure to publish your website </li>
 
             </ul>
 
