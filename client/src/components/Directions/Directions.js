4a74c8dcb16f8b5c322ceaf7d98db97914f709f5
Dan Arbelo <govepitr@gmail.com>
2021-05-24 10:05:47 -0700

    Updated directions positioning

--- client/src/components/Directions/Directions.js
+++ client/src/components/Directions/Directions.js
@@ -11,7 +11,7 @@ const Directions = () => {
           <div className="p-4 font-dark-blue raleway-font text-xl align-text-center">
             <h2>Click each section to enter portfolio information</h2>
             <ul className="">
-              <li><i>Starred</i> * fields are required</li>
+              <li className="text-center">Starred * fields are required</li>
               <li>When you are finished, click <b><i>Create Portfolio Zip File</i></b> to download zip files containing your HTML and CSS.</li>
               <li>Your portfolio assumes all images are located in an <b><i>assets/images</i></b> subfolder and that the css is in an <b><i>assets/css</i></b> subfolder. You must create this path structure to publish your website. </li>
               <li>Click <b><i>Detailed Directions</i></b> for more informaton</li>
